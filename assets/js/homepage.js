ab8d2e6130e968b247fc1a1dff7968234b64c44a
lillielove <lillielove@gmail.com>
2022-06-13 12:43:06 -0700

    allow user to search repos by language--html, css, and JS. add buttons.

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,5 +1,6 @@
 var userFormEl = document.querySelector("#user-form");
 var nameInputEl = document.querySelector("#username");
+var languageButtonEl = document.getElementById("language-buttons");
 
 var repoContainerEl = document.querySelector("#repos-container");
 var repoSearchTerm = document.querySelector("#repo-search-term");
@@ -66,6 +67,20 @@ var getUserRepos = function (user) {
 // repo name - name
 // how many issues - open_issues_count vs open_issues
 
+var getFeaturedRepos=function(language){
+    var apiUrl =  "https://api.github.com/search/repositories?q="+language+"is:featured&sort=help-wanted-issues";
+    fetch(apiUrl).then(function(response){
+        if(response.ok){
+            response.json().then(function(data){
+                displayRepos(data.items, language)
+            });
+        } else{
+            alert("Error: GitHub user not found");
+        }
+    });
+};
+
+
 var formSubmitHandler = function (event) {
     event.preventDefault();
     var username = nameInputEl.value.trim();
@@ -79,4 +94,16 @@ var formSubmitHandler = function (event) {
     }
 }
 
+var buttonSubmitHandler = function(event){
+    event.preventDefault();
+    var language = event.target.getAttribute("data-language");
+
+    console.log(language);
+    if(language){
+        getFeaturedRepos(language);
+        repoContainerEl.textContent="";
+    }
+}
+
 userFormEl.addEventListener("submit", formSubmitHandler);
+languageButtonEl.addEventListener("click",buttonSubmitHandler);
